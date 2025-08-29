6425e8badf10612f4f73888c5a3a1798a4f5e943
cpuuntery <57891661+cpuuntery@users.noreply.github.com>
2025-08-29 22:31:58 +0300

    Add files via upload

---
+++ Magisk_arm32.sh
@@ -0,0 +1,233 @@
+#!/bin/bash
+
+TARGET="$HOME/Magisk/pc_magisk/boot_patch.sh"
+
+if [[ -f "$TARGET" ]]; then
+
+  # Transfer execution flow to foobar.sh (replaces this process)
+    read -e -p "Drag & drop your boot.img : " file
+    eval file=$file
+    exec sh "$TARGET" $file
+fi
+
+# This runs only if foobar.sh doesn’t exist
+
+# Sudo check
+if [ `whoami` = root ];
+then
+    echo Please do not run this script as root or using sudo
+    return 1 2>/dev/null
+    exit 1
+fi
+
+# Functions 
+
+copy_files() {
+    echo ""
+    echo "Copying files"
+    cp $HOME/Magisk/wokdir/assets/boot_patch.sh $HOME/Magisk/pc_magisk/boot_patch.sh
+    cp $HOME/Magisk/wokdir/assets/util_functions.sh $HOME/Magisk/pc_magisk/util_functions.sh
+    cp $HOME/Magisk/wokdir/lib/x86_64/libmagiskboot.so $HOME/Magisk/pc_magisk/magiskboot
+    cp $HOME/Magisk/wokdir/assets/stub.apk $HOME/Magisk/pc_magisk/stub.apk
+    cp $HOME/Magisk/wokdir/lib/armeabi-v7a/libmagisk.so $HOME/Magisk/pc_magisk/magisk
+    cp $HOME/Magisk/wokdir/lib/armeabi-v7a/libmagiskinit.so $HOME/Magisk/pc_magisk/magiskinit
+    cp $HOME/Magisk/wokdir/lib/armeabi-v7a/libinit-ld.so $HOME/Magisk/pc_magisk/init-ld
+    cd $HOME/Magisk/pc_magisk/
+    chmod -R +x *
+    ./magiskboot compress=xz magisk magisk.xz
+    ./magiskboot compress=xz stub.apk stub.xz
+    ./magiskboot compress=xz init-ld init-ld.xz
+}
+
+install_dependencies() {
+    echo ""
+    echo "Installing dependencies"
+    echo ""
+    if [ -f /etc/os-release ]; then
+        . /etc/os-release
+        OS=$NAME
+    else
+        echo "Unsupported OS"
+        exit 1
+    fi
+    case $OS in
+        "Ubuntu" | "Linux Mint")
+            sudo apt update
+            sudo apt install dos2unix unzip ed curl -y
+            ;;
+        "Fedora")
+            sudo dnf install android-tools dos2unix unzip ed curl -y
+            ;;
+        "Arch Linux" | "Manjaro Linux")
+            sudo pacman -S android-tools dos2unix unzip ed curl --noconfirm
+            ;;
+        *)
+            echo "Unsupported OS: $OS"
+            exit 1
+            ;;
+    esac
+    PATH=$PATH:/usr/lib/android-sdk/platform-tools/fastboot
+    PATH=$PATH:/usr/lib/android-sdk/platform-tools/adb
+    programs=("dos2unix" "unzip" "curl" "ed")
+    for program in "${programs[@]}"; do
+        if sudo which "$program" >/dev/null 2>&1; then
+            echo "$program is installed"
+            echo ""
+        else
+            echo "$program is not installed"
+            sleep 10
+            exit 1
+        fi
+    done
+}
+adapt_the_script_for_pc() {
+    echo ""
+    echo "Adapting script for pc"
+    echo ""
+    
+    # Get line
+    line=$(grep -n '/proc/self/fd/$OUTFD' util_functions.sh | awk '{print $1}' | sed 's/.$//')
+
+    # Add echo "$1" and delete the line
+    (
+    echo "$line"
+    echo 'd'
+    echo "$line-1"
+    echo a
+    echo 'echo "$1"'
+    echo .
+    echo wq
+    ) | ed util_functions.sh > /dev/null 2>&1 
+
+    # Replace getprop
+    sed -i 's/getprop/adb shell getprop/g' util_functions.sh 
+
+    # Adb
+    echo "Waiting for adb conenction"
+}
+
+patch_the_image() {
+    # Patch
+    echo ""
+    echo "You need to accept the popup that appears on the phone"
+    echo ""
+    echo "Now if adb is working we can patch the image"
+    echo ""
+    read -e -p "Drag & drop your boot.img : " file
+    eval file=$file
+    sh boot_patch.sh $file
+}
+
+# Menu
+mainmenu() {
+    echo -ne "
+1) Use the last stable magisk 
+2) Use magisk canary
+0) Exit
+Choose an option:  "
+    read -r ans
+    case $ans in
+
+    1)
+            # Dependencies
+            install_dependencies
+            
+            # Delete old dir
+            echo "Deleteting old dir"
+            rm -rf $HOME/Magisk
+            
+            # Make a dir for download
+            mkdir $HOME/Magisk
+            cd $HOME/Magisk
+            
+            # Download lastest release
+            echo ""
+            echo "Downloading lastest magisk"
+            echo ""
+            wget $(curl -s https://api.github.com/repos/topjohnwu/Magisk/releases/latest | grep 'browser_download_url' | cut -d\" -f4)
+            
+            # Remove no needed apk
+            rm $HOME/Magisk/stub-release.apk
+            
+            # Unzip the apk on his directory
+            echo ""
+            echo "Unzipping"
+            echo ""
+            mkdir $HOME/Magisk/wokdir
+            unzip $HOME/Magisk/Magisk* -d $HOME/Magisk/wokdir
+            
+            # Create direcorty where file will be copied
+            mkdir $HOME/Magisk/pc_magisk
+            
+            # Copy all files needed
+            copy_files
+            
+            # Remove old dir
+            rm -rf $HOME/Magisk/wokdir
+            
+            # Enter into folder 
+            cd $HOME/Magisk/pc_magisk
+            
+            # Adapt_the_script_for_pc
+            adapt_the_script_for_pc
+            
+            # Patch
+            patch_the_image
+        ;;
+    2)
+            
+            # Dependencies
+            install_dependencies
+            
+            # Delete old dir
+            echo "Deleteting old dir"
+            rm -rf $HOME/Magisk
+            
+            # Make a dir for download
+            mkdir $HOME/Magisk
+            cd $HOME/Magisk
+            
+            # Download lastest release
+            echo ""
+            echo "Downloading lastest magisk"
+            echo ""
+            wget https://raw.githubusercontent.com/topjohnwu/magisk-files/canary/app-debug.apk
+            
+            # Unzip the apk on his directory
+            echo ""
+            echo "Unzipping"
+            echo ""
+            mkdir $HOME/Magisk/wokdir
+            unzip $HOME/Magisk/app-debug.apk -d $HOME/Magisk/wokdir
+            
+            # Create direcorty where file will be copied
+            mkdir $HOME/Magisk/pc_magisk
+            
+            # Copy all files needed
+            copy_files
+            
+            # Remove old dir
+            rm -rf $HOME/Magisk/wokdir
+            
+            # Enter into folder 
+            cd $HOME/Magisk/pc_magisk
+            
+            # Adapt_the_script_for_pc
+            adapt_the_script_for_pc
+            
+            # Patch
+            patch_the_image
+        ;;
+    0)      
+            echo "Bye bye."
+            exit 0
+            ;;
+    *)
+        echo "Wrong option."
+        mainmenu
+        ;;
+    esac
+}
+
+mainmenu
+
